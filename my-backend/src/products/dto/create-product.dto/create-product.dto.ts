687aeb68b930a00bddd43e44c9301a0d40a05dc0
pghuy1510 <phamhuyyk1510@gmail.com>
2026-03-27 08:22:04 +0700

    Update Create-product

--- my-backend/src/products/dto/create-product.dto/create-product.dto.ts
+++ my-backend/src/products/dto/create-product.dto/create-product.dto.ts
@@ -1,20 +1,27 @@
-import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
+    @Transform(({ value }) => value.trim())
+    @IsString()
   @IsNotEmpty()
   name: string;
 
+  @IsString()
   @IsNotEmpty()
   description: string;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   price: number;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   stock: number;
 
+  @Type(() => Number)
   @IsNumber()
   categoryId: number;
 }
